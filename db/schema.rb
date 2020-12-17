897713ec338f647d4b4d3f45c10439aa09421d7d
Alex Rogna <alexrogna@yahoo.com>
2020-12-17 14:27:51 +0000

    before rerun scaffold to fix published_ad typo

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_17_140454) do
-
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "published_ad"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "excerpt"
-    t.string "location"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
